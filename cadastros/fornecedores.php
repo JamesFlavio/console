28dde63937bbf445617e9d67a8310024dbdcb503
JamesFlavio <JamesFlavioNDC@Gmail.com>
2017-05-12 04:20:29 -0300

    Início do projeto

---
+++ cadastros/fornecedores.php
@@ -0,0 +1,102 @@
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Fornecedores
+                        </h1>
+                        <ol class="breadcrumb">
+                            <li>
+                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
+                            </li>
+                            <li class="active">
+                                <i class="fa fa-edit"></i> Fornecedores
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+                <div class="row">
+                    <div class="col-lg-12">
+
+						<div class="table-responsive">
+					
+							<form class="col-xs-6">
+							  <div class="input-group">
+								<input type="text" class="form-control" placeholder="CNPJ ou CPF">
+								<div class="input-group-btn">
+								  <button class="btn btn-default" type="submit">
+									<i class="glyphicon glyphicon-plus"></i>
+								  </button>
+								</div>
+							  </div>
+							</form>
+					
+							<form class="col-xs-6">
+							  <div class="input-group">
+								<input type="text" class="form-control" placeholder="Pesquisar">
+								<div class="input-group-btn">
+								  <button class="btn btn-default" type="submit">
+									<i class="glyphicon glyphicon-search"></i>
+								  </button>
+								</div>
+							  </div>
+							</form>
+
+						<table class="table table-striped" style="overflow: auto;">
+							<thead>
+							  <tr>
+								<th>ID</th>
+								<th>Razão Social\Nome</th>
+								<th>Nome Fantasia\Apelido</th>
+								<th>CNPJ\CPF</th>
+								<th>Cidade</th>
+								<th>UF</th>
+							  </tr>
+							</thead>
+							<tbody>
+							  <tr>
+								<td>1</td>
+								<td>John</td>
+								<td>Doe</td>
+								<td>john@example.com</td>
+								<td>Brasília</td>
+								<td>DF</td>
+							  </tr>
+							  <tr>
+								<td>2</td>
+								<td>Mary</td>
+								<td>Moe</td>
+								<td>mary@example.com</td>
+								<td>Brasília</td>
+								<td>DF</td>
+							  </tr>
+							  <tr>
+								<td>3</td>
+								<td>July</td>
+								<td>Dooley</td>
+								<td>july@example.com</td>
+								<td>Brasília</td>
+								<td>DF</td>
+							  </tr>
+							</tbody>
+						</table>
+						</div>
+						
+                    </div>
+					
+					<ul class="pager">
+					  <li class="previous"><a href="#">Previous</a></li>
+					  <li><a href="#">1</a></li>
+					  <li><a href="#">2</a></li>
+					  <li class="next"><a href="#">Next</a></li>
+					</ul>
+					<!-- /.pager -->
+ 
+				</div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
