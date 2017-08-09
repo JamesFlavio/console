72ecd04dd9e96f9f0b80c32eb5135c434a64ea1f
Leandro <Leandro@Leandro-PC>
2017-08-09 14:50:06 -0300

    Modificação na organização das tags.

--- manutencoes/vendas/venda.php
+++ manutencoes/vendas/venda.php
@@ -1,174 +1,164 @@
-<div class="col-lg-12" >
+<div class="col-xs-12">
 
-<?php
-
-// BUGS
-// 01:  
-		
-
-@$cmd 			= $_GET ["cmd"];
-@$id			= $_POST ["id"];
-
-
-switch ($cmd) {
-case "adicionar":
-	include("cadastros/produtos-e-servicos-adicionar-cmd.php");
-	break;
-};	
-
-?>
-	<div class="col-xs-12">
-	<button class="btn btn-default" type="button" onclick="linkModal('manutencoes/vendas/produto_incluir.php');">
-		<span class="glyphicon glyphicon-user"></span>
-		Alterar cliente
-	</button>
-	</div>
-
-<div class="form-group ">
-
-	<?php 
-
-	/**
-	 * Faz o insert no banco de uma nova venda ao cliente selecionado
-	 */
-	 $_SESSION['venda']=[
-		"cadastro_id"	=> "$id",
-		"data"			=> date("Y-m-d H:i:s")
-	 ];
-	 
-	 //var_dump($_SESSION['venda']);
-	 
-	 //echo $_SESSION['venda']['cadastro_id']." AQUI";
-
-	// SELECT
-	
-	include("class/BancoMysql.php");
-
-	$bd	= new BancoMysql();
-	$bd->setSelect("SELECT id, razao_social_ou_nome,cnpj_ou_cpf FROM cadastro WHERE id = '".$_SESSION['venda']['cadastro_id']."';");
-	
-	$rows	= $bd->getSelect();
-		
+    <?php
+        
+        // BUGS
+        // 01:  
+        		
+        
+        @$cmd 			= $_GET ["cmd"];
+        @$id			= $_POST ["id"];
+        
+        
+        switch ($cmd) {
+        case "adicionar":
+        	include("cadastros/produtos-e-servicos-adicionar-cmd.php");
+        	break;
+        };	
+    
+    	/**
+    	 * Faz o insert no banco de uma nova venda ao cliente selecionado
+    	 */
+    
+    	// SELECT
+    	
+    	include("class/BancoMysql.php");
+    
+    	$bd	= new BancoMysql();
+    	$bd->setSelect("SELECT id, razao_social_ou_nome,cnpj_ou_cpf FROM cadastro WHERE id = '".$_SESSION['venda']['cadastro_id']."';");
+    	
+    	$rows	= $bd->getSelect();
+    		
 		$id						= $rows['id'];
 		$razao_social_ou_nome	= $rows['razao_social_ou_nome'];
 		$cnpj_ou_cpf         	= $rows['cnpj_ou_cpf'];
-		
-	
-	/**
-	 * Imprime resultados
-	 */
-	 echo "
-	<div class='col-xs-12'>
-		<div class='col-xs-1'>ID</div>
-		<div class='col-xs-11'>$id</div>
-	</div>
-
-	<div class='col-xs-12'>
-		<div class='col-xs-1'>Razão Social</div>
-		<div class='col-xs-11'>$razao_social_ou_nome</div>
-	</div>
-
-	<div class='col-xs-12'>
-		<div class='col-xs-1'>CNPJ/CPF</div>
-		<div class='col-xs-11'>$cnpj_ou_cpf</div>
-	</div>
-
-	<div class='col-xs-12'>
-		<div class='col-xs-1'>Contato:</div>
-		<div class='col-xs-11'>Nome do contato</div>
-	</div>
-
-	<div class='col-xs-12'>
-		<div class='col-xs-1'>Telefone</div>
-		<div class='col-xs-11'>(61)3046-6866</div>
-	</div>
-         ";
 		 
-		?>
-</div>
+	?>
 
-<div class="col-xs-12">
-	<button class="btn btn-default" type="button" onclick="linkModal('manutencoes/vendas/produto_incluir.php');">
-		<i class="glyphicon glyphicon-ok"></i>
-		Incluir
-	</button>
+	<div class="row">
+    	<div class="col-xs-12">
+        	<button class="btn btn-default" type="button" onclick="linkModal('manutencoes/vendas/venda_cabecalho.php');">
+        		<span class="glyphicon glyphicon-user"></span>
+        		Alterar cliente
+        	</button>
+    	</div>
+	</div>
 
-	<button class="btn btn-default" type="submit">
-		<i class="glyphicon glyphicon-remove"></i>
-		Excluir Tudo
-	</button>
+	<div class="row">	
+		<div class='col-xs-1'>ID</div>
+		<div class='col-xs-10'><?php echo $id?></div>	
+	</div>
 
-	<button class="btn btn-default" type="submit">
-		<i class="glyphicon glyphicon-usd"></i>
-		Faturar
-	</button>
+
+	<div class="row">	
+		<div class='col-xs-1'>Razão Social</div>
+		<div class='col-xs-10'><?php echo $razao_social_ou_nome?></div>
+	</div>
+
+
+	<div class="row">	
+		<div class='col-xs-1'>CNPJ/CPF</div>
+		<div class='col-xs-10'><?php echo $cnpj_ou_cpf?></div>
+	</div>
+
+
+	<div class="row">	
+		<div class='col-xs-1'>Contato:</div>
+		<div class='col-xs-10'><?php echo "Nome do contato"?></div>
+	</div>
+
+	<div class="row">	
+		<div class='col-xs-1'>Telefone</div>
+		<div class='col-xs-10'><?php echo "(61)3046-6866"?></div>
+	</div>
+
+
+    <div class="row">
+		<div class="col-xs-12">
+        	<button class="btn btn-default" type="button" onclick="linkModal('manutencoes/vendas/produto_incluir.php');">
+        		<i class="glyphicon glyphicon-ok"></i>
+        		Incluir
+        	</button>
+
 	
-	<button class="btn btn-default" type="submit">
-		<i class="glyphicon glyphicon-file"></i>
-		Nota Fiscal
-	</button>
-	
-	<button class="btn btn-default" type="submit">
-		<i class="glyphicon glyphicon-floppy-disk"></i>
-		Salvar como
-	</button>
-</div>
+        	<button class="btn btn-default" type="submit">
+        		<i class="glyphicon glyphicon-remove"></i>
+        		Excluir Tudo
+        	</button>
+
+
+        	<button class="btn btn-default" type="submit">
+        		<i class="glyphicon glyphicon-usd"></i>
+        		Faturar
+        	</button>
+
+
+        	<button class="btn btn-default" type="submit">
+        		<i class="glyphicon glyphicon-file"></i>
+        		Nota Fiscal
+        	</button>
+		
+        	<button class="btn btn-default" type="submit">
+        		<i class="glyphicon glyphicon-floppy-disk"></i>
+        		Salvar como
+        	</button>
+		</div>
+    </div>
+    
 	<table class="table table-striped" style="overflow: auto;">
-		<thead>
-		  <tr>
-			<th class="col-sm-1">Cod.</th>
-			<th class="col-sm-5">Nome</th>
-			<th class="col-sm-2 text-center">Qnt</th>
-			<th class="col-sm-2">Preço Unt.</th>
-			<th class="col-sm-2">Total</th>
-			<th></th>
-		  </tr>
-		</thead>
-		<tbody>
-		
-		<?php
-		
-		$bdproduto = new BancoMysql();
-		$bdproduto->setSelect("
-			SELECT venda_item.produto_id, produto.nome, venda_item.quantidade, venda_item.valor_unitario
-			FROM venda_item
-			JOIN produto
-			ON produto.id = venda_item.produto_id
-			WHERE venda_id = '1'
-		;");
-		
-
-
-		// output data of each row
-		while($rowproduto = $bdproduto->getSelect()) {
-
-				// Faz captura de campos
-				$produto_id	= $rowproduto["produto_id"];
-				$nome					= $rowproduto["nome"];
-				$quantidade				= $rowproduto["quantidade"];
-				$valor_unitario			= $rowproduto["valor_unitario"];
-		
-		
-		?>
+        <tr>
+            <th class="col-sm-1">Cod.</th>
+            <th class="col-sm-4">Nome</th>
+            <th class="col-sm-2 text-center">Qnt</th>
+            <th class="col-sm-2">Preço Unt.</th>
+            <th class="col-sm-2">Total</th>
+            <th class="col-sm-1">Excluir</th>
+        </tr>
 
 		
-		  <tr>
-			<td>Produto <?php echo $produtosEServicos_id;?></td>
-			<td><?php echo $nome;?></td>
-			<td><?php echo $quantidade;?></td>
-			<td><?php echo $preco;?></td>
-			<td>Total <?php echo $quantidade*$preco;?></td>
-		  </tr>
+	<?php
+    	
+    	$bdproduto = new BancoMysql();
+    	$bdproduto->setSelect("
+    		SELECT venda_item.produto_id, produto.nome, venda_item.quantidade, venda_item.valor_unitario
+    		FROM venda_item
+    		JOIN produto
+    		ON produto.id = venda_item.produto_id
+    		WHERE venda_id = '1'
+    	;");
+    	
+    
+    
+    	// output data of each row
+    	while($rowproduto = $bdproduto->getSelect()) {
+    
+    			// Faz captura de campos
+    			$produto_id	= $rowproduto["produto_id"];
+    			$nome					= $rowproduto["nome"];
+    			$quantidade				= $rowproduto["quantidade"];
+    			$valor_unitario			= $rowproduto["valor_unitario"];
+    	
+    	
+	?>
 
-		<?php
+    
+        <tr>
+            <td>Produto <?php echo $produtosEServicos_id;?></td>
+            <td><?php echo $nome;?></td>
+            <td><?php echo $quantidade;?></td>
+            <td><?php echo $preco;?></td>
+            <td>Total <?php echo $quantidade*$preco;?></td>
+        </tr>
 
-		};
-        
-		?>
-		<tr>
-			<td>Produto </td>
-			<td>Nome</td>
-			<td>
+	<?php
+
+	   };
+    
+	?>
+        <tr>
+        	<td>Produto </td>
+        	<td>Nome</td>
+        	<td>
                 <div class="input-group form-group">
                 	<!-- <div class="input-group-btn">
                     	<button class="btn btn-default" type="submit">
@@ -183,22 +173,17 @@ case "adicionar":
                     		<i class="glyphicon glyphicon-plus"></i>
                     	</button>
                     </div> -->
-                </div>
-			</td>
-			<td>Preço</td>
-			<td>Total</td>
-			<td>	
-				<button class="btn btn-default" type="button">
-					<i class="glyphicon glyphicon-remove"></i>
-					Excluir
-				</button>
-			</td>
-		  </tr>
-		</tbody>
+                    </div>
+        	</td>
+        	<td>Preço</td>
+        	<td>Total</td>
+        	<td>	
+        		<button class="btn btn-default" type="button">
+        			<i class="glyphicon glyphicon-remove"></i>
+        			Excluir
+        		</button>
+        	</td>
+        </tr>
 	</table>
-
-<br>
 </div>
-<!-- /.row -->
-
 <script src="manutencoes/vendas/js/produtos-e-servicos-adicionar.js"></script>
