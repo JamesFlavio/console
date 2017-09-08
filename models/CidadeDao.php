c007b013dd9c6c0cf3cf3a1e737d57a6de4fb944
Leandro <Leandro@Leandro-PC>
2017-09-08 19:03:14 -0300

    Classe de acesso ao banco (Cidade).

---
+++ models/CidadeDao.php
@@ -0,0 +1,61 @@
+<?php
+
+class CidadeDao{
+
+    private $conn;
+    
+    /**
+     * Construtor com injeção de dependência.
+     *
+     * @param Connection $conn Conexão com o banco de dados.
+     */
+    public function __construct(Connection $conn){
+        $this->conn = $conn->getPdo();
+    }
+    
+    public function listByIbge($ibge){
+        try{
+            $stmt = $this->conn->prepare("SELECT * FROM cidade WHERE ibge LIKE ?");
+            $stmt->bindValue(1, "$ibge%");
+            if ($stmt->execute()) {
+                $stmt->setFetchMode(PDO::FETCH_CLASS, 'Cidade');
+                return $stmt->fetchAll();
+            }else {
+                throw new PDOException("Não foi possível executar a declaração sql.");
+            }
+        }catch(PDOException $erro){
+            echo "Erro: ".$erro->getMessage();
+        }
+    }
+    
+    public function listByNome($nome){
+        try{
+            $stmt = $this->conn->prepare("SELECT * FROM cidade WHERE nome LIKE ?");
+            $stmt->bindValue(1, "$nome%");
+            if ($stmt->execute()) {
+                $stmt->setFetchMode(PDO::FETCH_CLASS, 'Cidade');
+                return $stmt->fetchAll();
+            }else {
+                throw new PDOException("Não foi possível executar a declaração sql.");
+            }
+        }catch(PDOException $erro){
+            echo "Erro: ".$erro->getMessage();
+        }
+    }
+    
+    public function listByEstadoUf($estadoUf){
+        try{
+            $stmt = $this->conn->prepare("SELECT * FROM cidade WHERE estado_uf = ?");
+            $stmt->bindValue(1, "$estadoUf%");
+            if ($stmt->execute()) {
+                $stmt->setFetchMode(PDO::FETCH_CLASS, 'Cidade');
+                return $stmt->fetch();
+            }else {
+                throw new PDOException("Não foi possível executar a declaração sql.");
+            }
+        }catch(PDOException $erro){
+            echo "Erro: ".$erro->getMessage();
+        }
+    }
+}
+
